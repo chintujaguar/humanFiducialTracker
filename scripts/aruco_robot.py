bd1c672712f8937d8d666b9dbb52f463f15c434f
spanchal001 <spanchal@wpi.edu>
2021-12-11 16:01:51 -0500

    Sagar work

--- scripts/aruco_robot.py
+++ scripts/aruco_robot.py
@@ -11,12 +11,17 @@
 # Subscribe to /fiducial_transforms topic
 
 import rospy
+import numpy as np
+from tf.transformations import quaternion_matrix
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Transform
 from fiducial_msgs.msg import FiducialArray
 from fiducial_msgs.msg import FiducialTransformArray
 from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import CameraInfo
+
+global x_initial,z_initial
 
 def turt_vid_cb(msg):
     rospy.loginfo("SUBSCRIBING TO COMPRESSED IMAGE TOPIC")
@@ -33,8 +38,35 @@ def fid_vert_cb(msg):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
 
 def fid_trans_cb(msg):
-    rospy.loginfo("SUBSCRIBING TO FID TRANS TOPIC")
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+    # rospy.loginfo("SUBSCRIBING TO FID TRANS TOPIC")
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+    # print(msg.transforms[0].transform.translation.x)
+    # rotation_matrix = quaternion_matrix([msg.transforms[0].transform.rotation.x,msg.transforms[0].transform.rotation.y,msg.transforms[0].transform.rotation.z,msg.transforms[0].transform.rotation.w])
+    # print('Rotation Matrix= {}'.format(rotation_matrix))
+    # rotation_matrix = rotation_matrix[0:3,0:3]
+    # rotation_matrix_inv = np.linalg.pinv(rotation_matrix)
+    # print(rotation_matrix)
+    # xyz = np.c_[[msg.transforms[0].transform.translation.x,msg.transforms[0].transform.translation.y,msg.transforms[0].transform.translation.z]]
+    # # print('xyz={}'.format(xyz.shape))
+    # Translations = -1 * np.matmul(rotation_matrix_inv,xyz)
+    # print(Translations)
+    # H = np.concatenate([rotation_matrix_inv,Translations],axis = 1)
+    # row = np.array([[0,0,0,1]])
+    # coordinates = np.c_[[0,0,0,1]]
+    # # print(xyz.shape)
+    # # print(row.shape)
+    # H = np.concatenate([H,xyz], axis = 0)
+    # print(H)
+    # xyz_camera = np.matmul(rotation_matrix_inv,xyz)
+    # print(xyz_camera)
+    z = msg.transforms[0].transform.translation.z
+    x = msg.transforms[0].transform.translation.x
+    x_initial = 0
+    z_initial = 0
+    
+
+
+
     
 def aruco_get():
     rospy.init_node('aruco_robot', anonymous=True)
@@ -42,9 +74,9 @@ def aruco_get():
     global img
     # cam_info = rospy.Publisher('/camera_info', CameraInfo, queue_size=10)
     # img = rospy.Publisher('/camera/compressed', CompressedImage, queue_size=10)
-    rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, turt_vid_cb)
-    rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, turt_cam_info_cb)
-    rospy.Subscriber('/fiducial_vertices', FiducialArray, fid_vert_cb)
+    # rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, turt_vid_cb)
+    # rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, turt_cam_info_cb)
+    # rospy.Subscriber('/fiducial_vertices', FiducialArray, fid_vert_cb)
     rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, fid_trans_cb)
     rospy.spin()
 
--- scripts/basic_tracker.py
+++ scripts/basic_tracker.py
@@ -1,64 +1,126 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
-from std_msgs import String
-from geometry_msgs import Twist
-from fiducial_msgs import Fiducials
-from fiducial_msgs import FiducialTransforms
-from sensor_msgs import CompressedImage
-from sensor_msgs import CameraInfo
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from fiducial_msgs.msg import FiducialArray
+from fiducial_msgs.msg import FiducialTransformArray
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CameraInfo
 
 # Control class for controlling the movement of turtlebot
 class Control():
-    # Constructor fro Control class
+    # Constructor for Control class
     def __init__(self):
-        Twist self.velocity
+        self.velocity = Twist()
         self.direct = 0
-        cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        
-    # Set forward velocity of the robot, pass negative value to move backward
-    def set_vel(self, vel):
-        velocity.linear.x = vel
-        vel_str = "Moving with velocity %s" % vel
-        rospy.loginfo(vel_str)
-        cmd_vel.publish(velocity)
-        rate.sleep()
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.x_vert = 0
+        self.depth = 0
+        self.side = 0
+        rospy.Subscriber('/fiducial_vertices', FiducialArray, self.fid_vert_cb)
+        rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, self.fid_trans_cb)
 
-    # Set positive to take right turn, or negative to take a left turn
-    def set_dir(self, dir, angle):
-        if dir == "left":
-            self.direct = -1
+    def infinite_loop(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            print("depth in main: ", self.depth)
+            self.set_dir(self.x_vert)
+            self.set_vel(self.depth)
+            hello_str = "hello world %s" % rospy.get_time()
+            rospy.loginfo(hello_str)
+            # pub.publish(hello_str)
+            rate.sleep()
+
+    # Set forward velocity of the robot based on x value
+    def set_vel(self, x):
+        print("depth in set_vel: ", x)
+        if x > 5:
+            self.velocity.linear.x = 2
+        elif x < 0.75:
+            self.set_vel_zero()
+        else:  
+            self.velocity.linear.x = 0.1 # If possible, make this smoother by changing velocity according to x value
+        self.vel_str = "Moving with velocity %s" % self.velocity.linear.x
+        rospy.loginfo(self.vel_str)
+        self.cmd_vel.publish(self.velocity)
+
+    # Set angular velocity based on y coordinate of aruco top left vertex in image frame
+    def set_dir(self, y):
+        # wheel_base = 0.3
+        if y < 620: # Object to the left
+            self.velocity.angular.z = 0.25
+        elif y > 630: # Object to the right
+            self.velocity.angular.z = -0.25
         else:
-            self.direct = 1
-        velocity.angular.z = direct*angle
-        vel_str = "Moving in direction" + dir
+            self.set_vel_zero()
+        vel_str = "Moving in direction" # + dir
         rospy.loginfo(vel_str)
-        vel_str = "with angular velocity %s" % angle
+        vel_str = "with angular velocity %s" #% angle
         rospy.loginfo(vel_str)
-        cmd_vel.publish(velocity)
+        self.cmd_vel.publish(self.velocity)
 
     # Set velocity zero to stop robot
-    def set_vel_zero():
-        velocity.linear.x = 0
+    def set_vel_zero(self):
+        self.velocity.linear.x = 0
+        self.velocity.angular.z = 0
         vel_str = "Stopping the robot"
         rospy.loginfo(vel_str)
-        cmd_vel.publish(velocity)
+        self.cmd_vel.publish(self.velocity)
+
+    def fid_vert_cb(self, msg):
+        # rospy.loginfo("SUBSCRIBING TO FID VERT TOPIC")
+        fid_array = FiducialArray()
+        fid_array = msg
+        if len(fid_array.fiducials) >= 1:
+            self.x_vert = fid_array.fiducials[0].x0
+        else:
+            self.x_vert = 625
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+
+    def fid_trans_cb(self, msg):
+        # rospy.loginfo("SUBSCRIBING TO FID TRANS TOPIC")
+        transform = FiducialTransformArray()
+        transform = msg
+        if len(transform.transforms) >= 1:
+            self.depth = transform.transforms[0].transform.translation.z
+            self.side = transform.transforms[0].transform.translation.x
+        else:
+            self.depth = 0.1
+            self.side = 0
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+
+# def aruco_get():
+#     rospy.Subscriber('/fiducial_vertices', FiducialArray, fid_vert_cb)
+#     rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, fid_trans_cb)
 
 def main():
     rospy.init_node('turtle_controller', anonymous=True)
-    tc = Control()
-    tc.set_dir("left", 1)
-    tc.set_vel(1)
-
+    # aruco_get()
     rate = rospy.Rate(10) # 10 Hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
-        while not rospy.is_shutdown():
+    tc = Control()
+    # cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    # velocity = Twist()
+    # velocity.linear.x = 0.1
+    # velocity.angular.z = 0.1
+    # cmd_vel.publish(velocity)
+    print('Before while')
+    tc.infinite_loop()
+    # while not rospy.is_shutdown():
+    #     print("depth in main: ", depth)
+    #     # tc.set_dir(x_vert)
+    #     tc.set_vel(depth)
+    #     hello_str = "hello world %s" % rospy.get_time()
+    #     rospy.loginfo(hello_str)
+    #     # pub.publish(hello_str)
+    #     rate.sleep()
+    
+    rospy.spin()
 
-
-if __name__ == "__main__"
+if __name__ == '__main__':
+    # global depth
+    # global side
+    # x_vert = 0
+    # depth = 0
     main()
 
 
@@ -79,3 +141,11 @@ if __name__ == "__main__"
 #         talker()
 #     except rospy.ROSInterruptException:
 #         pass
+
+###### NOTES ######
+# The camera tf frame:
+# x points outwards
+# y points to the left
+# z points upwards
+
+# 624 (x) x 188 (y) is the center point of the image
